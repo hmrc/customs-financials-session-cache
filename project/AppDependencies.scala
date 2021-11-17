126f3731ba9f914eae6109ed7af052cd251dfd20
Derek Palmer <35488891+derekspalmer@users.noreply.github.com>
2021-11-17 11:29:25 +0000

    MMCA-3264: upgrade plugins/libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,8 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.3.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.50.0"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.16.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.56.0"
   )
 
   val test = Seq(
@@ -15,6 +15,6 @@ object AppDependencies {
     "com.typesafe.play" %% "play-test" % current % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test",
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test",
-    "org.mockito" %% "mockito-scala-scalatest" % "1.16.37" % "test"
+    "org.mockito" %% "mockito-scala-scalatest" % "1.16.46" % "test"
   )
 }
