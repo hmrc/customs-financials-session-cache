a5dee3164dfd5f45b1d3c566bdb9b02634f12978
AdamCoops <adam.cooper2@digital.hmrc.gov.uk>
2026-05-12 15:57:45 +0100

    DCWL-4468-update mongo vers

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   val bootstrapVersion     = "10.7.0"
-  private val mongoVersion = "2.6.0"
+  private val mongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
